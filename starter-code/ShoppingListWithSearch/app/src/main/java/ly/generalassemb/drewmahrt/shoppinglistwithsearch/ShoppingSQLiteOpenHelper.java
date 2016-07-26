3dd29c352df53246a274300625bdd7edcf0e88c8
Jane Ruby <janerubyskywalker@gmail.com>
2016-07-25 20:59:45 -0400

    lab

--- starter-code/ShoppingListWithSearch/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistwithsearch/ShoppingSQLiteOpenHelper.java
+++ starter-code/ShoppingListWithSearch/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistwithsearch/ShoppingSQLiteOpenHelper.java
@@ -16,45 +16,40 @@ import java.io.InputStreamReader;
 
 
 public class ShoppingSQLiteOpenHelper extends SQLiteOpenHelper{
-    private static final String TAG = ShoppingSQLiteOpenHelper.class.getCanonicalName();
-
-    private static final int DATABASE_VERSION = 7;
-    public static final String DATABASE_NAME = "SHOPPING_DB";
-    public static final String SHOPPING_LIST_TABLE_NAME = "SHOPPING_LIST";
-
-    public static final String COL_ID = "_id";
-    public static final String COL_ITEM_NAME = "ITEM_NAME";
-    public static final String COL_ITEM_PRICE = "PRICE";
-    public static final String COL_ITEM_DESCRIPTION = "DESCRIPTION";
-    public static final String COL_ITEM_TYPE = "TYPE";
 
 
-    public static final String[] SHOPPING_COLUMNS = {COL_ID,COL_ITEM_NAME,COL_ITEM_DESCRIPTION,COL_ITEM_PRICE,COL_ITEM_TYPE};
+    public Cursor showAllGroceries() {
+               SQLiteDatabase db = getReadableDatabase();
 
-    private static final String CREATE_SHOPPING_LIST_TABLE =
-            "CREATE TABLE " + SHOPPING_LIST_TABLE_NAME +
-                    "(" +
-                    COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    COL_ITEM_NAME + " TEXT, " +
-                    COL_ITEM_DESCRIPTION + " TEXT, " +
-                    COL_ITEM_PRICE + " REAL, " +
-                    COL_ITEM_TYPE + " TEXT )";
+                Cursor cursor = db.query(SHOPPING_LIST_TABLE_NAME,
+                                new String[]{COL_ID,COL_ITEM_NAME},
+                                null,
+                                null,
+                                null, null, null);
 
-
-    public ShoppingSQLiteOpenHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+                        return cursor;
     }
+
+                public Cursor searchGroceryNames(String query) {
+                SQLiteDatabase db = getReadableDatabase();
+
+                        Cursor cursor = db.query(SHOPPING_LIST_TABLE_NAME,
+                                new String[]{COL_ID,COL_ITEM_NAME},
+                                COL_ITEM_NAME + " LIKE + ?",
+                                new String[]{"%"+query+"%"},
+                                null, null, null);
+
+                        return cursor;
+            }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_SHOPPING_LIST_TABLE);
+        db.execSQL(SHOPPING_LIST_TABLE_NAME);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + SHOPPING_LIST_TABLE_NAME);
-        this.onCreate(db);
-    }
 
+    }
 
 }
